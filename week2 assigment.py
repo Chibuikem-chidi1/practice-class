6ffee503a83452ef0cf500b8f66b572d770ab5ef
chidi chibuikem <chidichibuikem14@gmail.com>
2024-03-01 16:45:28 +0100

    weekly_challenge

---
+++ week2 assigment.py
@@ -0,0 +1,18 @@
+my_list = []
+print(my_list)
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list.insert(2, 15)
+print(my_list)
+my_list = [10, 20, 15, 30, 40]
+another_list = [50, 60, 70]
+my_list.extend(another_list)
+print(my_list)
+my_list.remove(70)
+print(my_list)
+my_list.sort()
+print(my_list)
+print(my_list[3])
